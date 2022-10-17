6cfa3fc34956333e6bbfab48b61e61b1d99ccc35
Timothy Maina Karanja <tkaranja@safaricom.co.ke>
2022-10-17 16:05:14 +0300

    Get Day of the week

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function myFunction() {
-    // alert("Hello World");
+function getAkanName() {
+    let day = document.getElementsByName("day")[0].value;
+    let month = document.getElementsByName("month")[0].value;
+    let year = document.getElementsByName("year")[0].value;
+
+
+    days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    var date = new Date(year + "-" + month + "-" + day);
+    let dayOfWeek = date.getDay()
+
+
+    console.log(days[dayOfWeek]);
+
+
+    
+
 }
