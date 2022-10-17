bc37b56a0460de81e66fed6dd23a54fafc3bef46
Timothy Maina Karanja <tkaranja@safaricom.co.ke>
2022-10-17 16:25:30 +0300

    display the greeting

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getAkanName() {
+
+    let gender = document.getElementsByName("gender")[0].value;
     let day = document.getElementsByName("day")[0].value;
     let month = document.getElementsByName("month")[0].value;
     let year = document.getElementsByName("year")[0].value;
@@ -10,6 +12,14 @@ function getAkanName() {
 
     var date = new Date(year + "-" + month + "-" + day);
     let dayOfWeek = date.getDay()
+    let akanName;
 
+    if (gender === "male"){
+        akanName = maleNames[dayOfWeek];
+    }else{
+        akanName = femaleNames[dayOfWeek];
+    }
+    let result = "Hi, your akan name : " + akanName + " and you were born on: " + days[dayOfWeek];
+    document.getElementsByClassName("result")[0].innerHTML = result;
 
 }
